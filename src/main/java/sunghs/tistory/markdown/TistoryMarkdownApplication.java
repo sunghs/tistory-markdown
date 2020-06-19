28fbe2bb786527737868d756f7c8402974cda64a
sunghs <sunghs9071@naver.com>
2020-06-19 21:23:55 +0900

    이니셜 커밋

---
+++ src/main/java/sunghs/tistory/markdown/TistoryMarkdownApplication.java
@@ -0,0 +1,13 @@
+package sunghs.tistory.markdown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TistoryMarkdownApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TistoryMarkdownApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sunghs/tistory/markdown/TistoryMarkdownApplicationTests.java
@@ -0,0 +1,13 @@
+package sunghs.tistory.markdown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TistoryMarkdownApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
